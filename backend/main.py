35a1ad57bdb5a5b56c3e49cea56ae35eac1ec9bb
Pasin Makcharoen <pasin0931@Pasins-MacBook-Air-2.local>
2026-09-04 00:02:58 +0700

    FEAT: Add nextresponse api to get frontend .env and edit the folder structure

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from auth import router as auth_router
+from starlette.middleware.sessions import SessionMiddleware
+import os
+from database import base, engine
+import models
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=os.getenv("JWT_SECRET_KEY"))
+
+base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
--- coursehub/src/components/landing.tsx
+++ coursehub/src/components/landing.tsx
@@ -8,15 +8,25 @@ import { Button } from "@/components/ui/button"
 import { FcGoogle } from "react-icons/fc"
 
 export default function LandingPage() {
+    const handle_submit = () => {
+        try {
+            window.location.href = "/api/auth"
+        }
+        catch (error) {
+            alert("Error while trting to login")
+            console.error(error)
+        }
+    }
+
     return (
         <div className="flex flex-row items-center h-screen">
             <div className="self-start w-8 bg-[#006C67] h-screen fixed" />
             <div className="flex flex-col items-center justify-center pl-25">
                 <h1 className="font-bold text-[72px] self-start">CourseHub</h1>
                 <h2 className="w-160 text-[20px]">A platform that centralizes course tasks, announcements, and FAQs to keep lecturers, TAs, and student in sync.</h2>
-                <Button className="self-start px-4 mt-8 gap-3 font-bold h-9">
+                <Button className="self-start px-4 mt-8 gap-3 font-bold h-9" onClick={handle_submit}>
                     <FcGoogle />
-                    Login with Google
+                    Continue with Google
                 </Button>
             </div>
             <div className="relative h-screen flex-1 ml-25">
