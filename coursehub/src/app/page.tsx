f47e3aaf8dd035f5559a1df594d978541258a46e
Pasin Makcharoen <pasin0931@Pasins-MacBook-Air-2.local>
2026-09-03 11:16:16 +0700

    Implement landing page ui

--- coursehub/src/app/page.tsx
+++ coursehub/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react"
 
-import LandingPage from "@/components/landingpage"
+import LandingPage from "@/components/landing"
 
 export default function Home() {
     return (
---
+++ coursehub/src/components/landing.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import Image from "next/image"
+
+import { Button } from "@/components/ui/button"
+
+import { FcGoogle } from "react-icons/fc"
+
+export default function LandingPage() {
+    return (
+        <div className="flex flex-row items-center h-screen">
+            <div className="self-start w-8 bg-[#006C67] h-screen fixed" />
+            <div className="flex flex-col items-center justify-center pl-25">
+                <h1 className="font-bold text-[72px] self-start">CourseHub</h1>
+                <h2 className="w-160 text-[20px]">A platform that centralizes course tasks, announcements, and FAQs to keep lecturers, TAs, and student in sync.</h2>
+                <Button className="self-start px-4 mt-8 gap-3 font-bold h-9">
+                    <FcGoogle />
+                    Login with Google
+                </Button>
+            </div>
+            <div className="relative h-screen flex-1 ml-25">
+                <Image
+                    src="/classroom_img.jpeg"
+                    alt="classroom"
+                    fill
+                    className="object-cover"
+                    priority
+                />
+            </div>
+        </div>
+    )
+}
