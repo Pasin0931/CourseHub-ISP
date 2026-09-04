9c8532508a0865ab737fe9e80c4abe5e5773c706
Pasin Makcharoen <pasin0931@Pasins-MacBook-Air-2.local>
2026-09-04 14:41:45 +0700

    FEAT: Implement placeholder for dashboard and calendar page inside a course

---
+++ coursehub/src/app/courses/[id]/calendar/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
+import CalendarPage from "@/components/calendar"
+
+export default function CalendarScreen() {
+    return (
+        <CalendarPage />
+    )
+}
